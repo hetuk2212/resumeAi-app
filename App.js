4760e92ade0087fbf86ef774929f89952fc80c24
hetuk2212 <hetuk2212@gmail.com>
2025-05-16 07:37:21 +0530

    ss

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import MainNavigation from './src/Navigation/MainNavigation'
+import React, { useEffect } from 'react';
+import { LogBox } from 'react-native';
+import MainNavigation from './src/Navigation/MainNavigation';
 import Toast from 'react-native-toast-message';
+import InAppUpdates, { IAUUpdateKind } from 'sp-react-native-in-app-updates';
 
 const App = () => {
+  const inAppUpdates = new InAppUpdates(true); // Enable debug logs
+
+  useEffect(() => {
+    const checkUpdate = async () => {
+      try {
+        const result = await inAppUpdates.checkNeedsUpdate();
+        if (result.shouldUpdate) {
+          await inAppUpdates.startUpdate({
+            updateType: IAUUpdateKind.IMMEDIATE, // or FLEXIBLE
+          });
+        }
+      } catch (e) {
+        console.log('In-app update error:', e);
+      }
+    };
+
+    checkUpdate();
+    LogBox.ignoreLogs(['new NativeEventEmitter']); // Optional: ignore warning
+  }, []);
+
   return (
     <>
-    <MainNavigation/>
-    <Toast/>
+      <MainNavigation />
+      <Toast />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
